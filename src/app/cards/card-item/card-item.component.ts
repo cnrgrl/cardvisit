f8cdd7607f7007cff8720f8a572c50a727f1d917
caner <caner@quizco.de>
2022-11-24 00:56:06 +0100

    card-item component created

---
+++ src/app/cards/card-item/card-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-item',
+  templateUrl: './card-item.component.html',
+  styleUrls: ['./card-item.component.scss'],
+})
+export class CardItemComponent implements OnInit {
+  @Input() cards: any;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CardsRoutingModule } from './cards-routing.module';
 import { CardsComponent } from './cards.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { CardItemComponent } from './card-item/card-item.component';
 
 @NgModule({
-  declarations: [
-    CardsComponent
-  ],
-  imports: [
-    CommonModule,
-    CardsRoutingModule
-  ]
+  declarations: [CardsComponent, CardItemComponent],
+  imports: [CommonModule, CardsRoutingModule, MatCardModule],
 })
-export class CardsModule { }
+export class CardsModule {}
