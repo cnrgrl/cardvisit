583e2487c091d9d468ab6e04ccd574dfdbc666e1
caner <66187631+cnrgrl@users.noreply.github.com>
2023-12-01 21:13:20 +0100

    Matdialogue

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-cards',
@@ -14,7 +15,7 @@ export class CardsComponent implements OnInit {
     address: 'Hanover/Germany',
   };
 
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {}
 }
--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -5,9 +5,10 @@ import { CardsRoutingModule } from './cards-routing.module';
 import { CardsComponent } from './cards.component';
 import { MatCardModule } from '@angular/material/card';
 import { CardItemComponent } from './card-item/card-item.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [CardsComponent, CardItemComponent],
-  imports: [CommonModule, CardsRoutingModule, MatCardModule],
+  imports: [CommonModule, CardsRoutingModule, MatCardModule, MatDialogModule],
 })
 export class CardsModule {}
